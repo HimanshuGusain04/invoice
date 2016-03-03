0c19bc6f6f1715830c081e80d96a2136d52698e7
Steven Jackson <stevenjackson.sanguine@gmail.com>
2016-03-03 18:03:02 +0200

    Factory created to return API json data

--- public/app/controllers/latestWorkCtrl.js
+++ public/app/controllers/latestWorkCtrl.js
@@ -1,19 +1,22 @@
 var app = angular.module('LatestWorkCtrlModule',['invoiceService']);
 
-app.controller('LatestWorkCtrl', ['$scope','$http', function($scope, $http) {
+app.controller('LatestWorkCtrl', function($scope, Invoice) {
   
  $scope.User = {};
         $scope.errorMessage = '';
 
 $scope.name = "testsasd";
-     // when landing on the page, get all todos and show them
-    $http.get('api/invoices')
-        .success(function(data) {
+
+Invoice.all()
+.success(function(data) {
             $scope.invoices = data;
             console.log(data);
         })
         .error(function(data) {
             console.log('Error: ' + data);
         });
+     // when landing on the page, get all todos and show them
+    // $http.get('api/invoices')
+        
  
-}]);
+});
